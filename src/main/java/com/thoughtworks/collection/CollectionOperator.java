9e17e9d3241a1205e61cdf955a8a584ef3943183
Ahn <gan_jinan@qq.com>
2019-07-15 00:38:11 +0800

    [feat][ add the implements of CollectionOperator for the CollectionOperatorTest.]

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,52 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        return left < right ?
+                IntStream.rangeClosed(left, right)
+                .boxed()
+                .sorted()
+                .collect(toList())
+                :
+                IntStream.rangeClosed(right, left)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        return left < right ?
+                IntStream.rangeClosed(left, right)
+                        .filter(v -> v % 2 == 0)
+                        .boxed()
+                        .sorted()
+                        .collect(toList())
+                :
+                IntStream.rangeClosed(right, left)
+                        .filter(v -> v % 2 == 0)
+                        .boxed()
+                        .sorted(Comparator.reverseOrder())
+                        .collect(toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return IntStream.of(array)
+                .filter(v -> v % 2 == 0)
+                .boxed()
+                .collect(toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return IntStream.of(array)
+                .reduce((prxNum ,nextNum) -> nextNum)
+                .getAsInt();
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
